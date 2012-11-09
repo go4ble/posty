a068c1265107a1e4f890fa3ffdff07d0a03d6a28
Kyle <go4ble23@gmail.com>
2012-11-09 15:31:34 -0800

    now just need to implement 'send' and 'update'

--- chat/views.py
+++ chat/views.py
@@ -1,1 +1,25 @@
-# Create your views here.
+import models
+from django.shortcuts import render
+
+class Chat(object):
+    def __init__(self):
+        pass
+
+    def index(self, request):
+        form = models.MsgForm()
+        msg_list = models.Msg.objects.all()
+        return render(request, 'index.html', {
+            'form': form,
+            'msg_list': msg_list,
+        })
+
+    def send(self, request):
+        pass
+
+    def update(self, request):
+        pass
+
+chat = Chat()
+index = chat.index
+send = chat.send
+update = chat.update
